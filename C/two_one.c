047203a1485b31c5500fffb81f0e986c06e4c967
Psoyeon <128681094+Psoyeon1117@users.noreply.github.com>
2024-05-22 01:52:53 +0900

    Update two_one.c

--- C/two_one.c
+++ C/two_one.c
@@ -17,21 +17,6 @@ void MALLOC(treepointer* node, int size) {
 	*node = malloc(size); // 포인터가 가리키는 곳에 메모리 할당 후 값을 저장
 }
 
-treepointer search(treepointer tree, int k) {
-	treepointer lastNode = NULL; // 마지막으로 검사된 노드를 가리키는 포인터
-	while (tree) {
-		if (k == tree->data.key)
-			return lastNode;	//삭제할 노드의 부모노드 반환
-		lastNode = tree;
-		if (k < tree->data.key) {
-			tree = tree->leftchild;
-		}
-		else {
-			tree = tree->rightchild;
-		}
-	}
-	return NULL;	//k와 일치하는 노드가 없다면 null을 반환
-}
 
 treepointer modifiedSearch(treepointer node, int k) {
 	treepointer last_examined_node = NULL; // 마지막으로 검사된 노드를 가리키는 포인터
@@ -79,34 +64,28 @@ void inorder(treepointer root) {
 		inorder(root->rightchild);
 	}
 }
-treepointer miniSearch(treepointer node) {
-	treepointer parent = node;
-	while (node->leftchild) {
-		parent = node;
-		node = node->leftchild;
-	}
-	parent->leftchild = NULL;
-	return node;
-}
 
-int delete(treepointer *node, int k) {
+int delete(treepointer* node, int k) {
 	//search함수로 트리에 k노드가 존재하는지 확인
-	treepointer temp = search(*node, k);
-	if (!temp)return NULL;	//존재하지 않는다면 null반환
+	treepointer temp = NULL;
 	treepointer ptr;		//ptr=삭제할 노드, temp=삭제노드의 parent
 	MALLOC(&ptr, sizeof(*ptr));
-	if (temp->leftchild->data.key == k) {	
-		ptr = temp->leftchild;
-	}else{
-		ptr = temp->rightchild;
+	ptr = *node;
+	while (ptr && ptr->data.key != k) {
+		temp = ptr;
+		ptr = (k < ptr->data.key) ? ptr->leftchild : ptr->rightchild;
 	}
+	if (ptr == NULL)return NULL;
 	/*삭제 노드 with child 1
 	삭제 노드와 부모 노드의 방향대로 삭제 노드의 부모 노드와 자식노드를 연결하면 됨*/
-	if (!(ptr->leftchild)||!(ptr->rightchild)) {
-		treepointer child = !(ptr->leftchild) ? ptr->leftchild : ptr->rightchild;	//삭제할 노드의 자식노드 정하기
-		if (temp->leftchild == ptr)temp->leftchild = child;
-		//ptr이 temp의 왼쪽에 있다면, temp의 leftchild와 ptr의 child가 연결됨
-		else temp->rightchild = child;	//오른쪽이라면, temp의 rightchild와 ptr의 child가 연결됨
+	if (!(ptr->leftchild) || !(ptr->rightchild)) {
+		treepointer child = (ptr->leftchild != NULL) ? ptr->leftchild : ptr->rightchild;	//삭제할 노드의 자식노드 정하기
+		if (temp) {	//부모 노드가 있다면
+			if (temp->leftchild == ptr)temp->leftchild = child;
+			//ptr이 temp의 왼쪽에 있다면, temp의 leftchild와 ptr의 child가 연결됨
+			else temp->rightchild = child;	//오른쪽이라면, temp의 rightchild와 ptr의 child가 연결됨
+		}
+		else { *node = child; }	//없다면, 삭제노드가 root이다.
 		free(ptr);
 		return k;
 	}
@@ -116,7 +95,7 @@ int delete(treepointer *node, int k) {
 		//smallest right subtree node를 제거한다. 삭제할 노드의 data는 반환
 		treepointer parent = ptr;
 		treepointer miniNode = ptr->rightchild;	//smallest right subtree node
-		while (miniNode->leftchild) {			
+		while (miniNode->leftchild) {
 			parent = miniNode;
 			miniNode = miniNode->leftchild;	//가장 작은 노드를 찾기 때문에 leftchild로 이동함
 		}
@@ -130,7 +109,7 @@ int delete(treepointer *node, int k) {
 		free(ptr);
 		return k;
 	}
-	
+
 }
 int main(void) {
 	//key==x 노드를 삭제하는 함수를 작성
@@ -147,4 +126,3 @@ int main(void) {
 	printf("\ndelete %d\n", k);
 	inorder(tree);
 }
-
